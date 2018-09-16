21312db67bc561f74b993ed3bf7122488b82ddf1
baarath <baarathsrinivas@gmail.com>
2018-09-16 23:59:01 +0530

    17 sept

--- bank.py
+++ bank.py
@@ -1,6 +1,6 @@
 from Tkinter import *
 class UI:
-    Frames = []
+    Frames = {}
     Window = None
     def __init__(self):
         Window = Tk()
@@ -31,7 +31,73 @@ class UI:
         Quit.bind("<Button-1>", Quitwindow)
         Quit.grid(row = 5, column=2)
 
-        self.Frames.append(frame)
+        self.Frames["main"] = frame
 
     def signinframe(self):
-        
+        frame = Frame(Window)
+        frame.grid(row=0,column=0)
+        Label(frame , text="Login" ,fg="red").grid(row=0 , column=2)
+        Label(frame , text="username").grid(row=2, column=0)
+        Label(frame ,text="password").grid(row =3, column=0)
+        username = Entry(frame, width=20)
+        password = Entry(frame , width=20)
+        username.grid(row=2, column=1)
+        passowrd.grid(row=3, column=1)
+
+        login = Button(frame ,text = "login")
+        login.bind("<Button-1>", loginto)
+        login.grid(row = 4, column=1)
+
+        self.Frames["signin"] = frame
+
+    def signupframe(self):
+        frame = Frame(Window)
+        frame.grid(row=0,column=0)
+        Label(frame , text="signup" ,fg="red").grid(row=0 , column=2)
+        Label(frame , text="username").grid(row=2, column=0)
+        Label(frame ,text="Date of birth").grid(row =3, column=0)
+        Label(frame ,text="password").grid(row =4, column=0)
+        username = Entry(frame, width=20)
+        dob = Entry(frame , width=20)
+        password = Entry(frame , width=20)
+        username.grid(row=2, column=1)
+        dob.grid(row=3, column=1)
+        passowrd.grid(row=3, column=1)
+
+        submit = Button(frame ,text = "submit")
+        submit.bind("<Button-1>", showsignin)
+        submit.grid(row = 4, column=1)
+
+        self.Frames["signup"] = frame
+
+     def adminframe(self):
+        frame = Frame(Window)
+        frame.grid(row=0,column=0)
+        Label(frame , text="Login" ,fg="red").grid(row=0 , column=2)
+        Label(frame , text="username").grid(row=2, column=0)
+        Label(frame ,text="password").grid(row =3, column=0)
+        username = Entry(frame, width=20)
+        password = Entry(frame , width=20)
+        username.grid(row=2, column=1)
+        passowrd.grid(row=3, column=1)
+
+        login = Button(frame ,text = "login")
+        login.bind("<Button-1>", showsubmenu)
+        login.grid(row = 4, column=1)
+
+        self.Frames["admin"] = frame
+
+    def showsignup(event):
+        self.Frame["signup"].tkraise()
+
+    def showsignin(event):
+        self.Frame["signin"].tkraise()
+
+    def showadmin(event):
+        self.Frame["admin"].tkraise()
+
+    def Quitwindow(event):
+        window.destory()
+
+    def loginto(event):
+        checkdb()
