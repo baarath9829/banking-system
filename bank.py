f6d579bbf28cf7280411f650b52e2b4f89098cfa
baarath <baarathsrinivas@gmail.com>
2018-09-26 13:14:13 +0530

    version1

--- bank.py
+++ bank.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+import random
 import cx_Oracle
 class UI:
     Frames = {}
@@ -55,6 +56,9 @@ class UI:
         #login.bind("<Button-1>", self.loginto)
         login.grid(row = 4, column=1)
 
+        back = Button(frame ,text = "back", command= lambda: self.showmainframe())
+        back.grid(row = 8, column=1)
+
         self.Frames["signin"] = frame
 
     def signupframe(self):
@@ -86,6 +90,9 @@ class UI:
         #submit.bind("<Button-1>", self.showsignin)
         submit.grid(row = 9, column=1)
 
+        back = Button(frame ,text = "back", command= lambda: self.showmainframe())
+        back.grid(row = 8, column=1)
+        
         self.Frames["signup"] = frame
 
     def adminframe(self): 
@@ -104,35 +111,41 @@ class UI:
         #login.bind("<Button-1>", self.adminlogin(username, password))
         login.grid(row = 4, column=1)
 
+        back = Button(frame ,text = "back", command= lambda: self.showmainframe())
+        back.grid(row = 8, column=1)
+        
         self.Frames["admin"] = frame
 
     def submenuframe(self):
         frame = Frame(self.Window, width=768, height=576 , bg="ivory")
         frame.grid(row=0,column=0)
         frame.grid_propagate(0)
-        Label(frame , text="SubMenu" ,fg="red").grid(row=0 , column=2)
+        Label(frame , text="SubMenu" ,fg="red").grid(row=0 , column=3)
 
-        addresschange = Button(frame ,text = "Address Change", command= lambda: self.changeaddress())
+        addresschange = Button(frame ,text = "Address Change", command= lambda: self.addressframe())
         addresschange.grid(row = 2, column=2)
 
-        deposit = Button(frame ,text = "Money Deposit", command= lambda: self.deposit())
-        deposit.grid(row = 3, column=2)
+        deposit = Button(frame ,text = "Money Deposit", command= lambda: self.depositframe())
+        deposit.grid(row = 2, column=4)
 
-        withdraw = Button(frame ,text = "Money Withdrawal", command= lambda: self.withdraw())
-        withdraw.grid(row = 4, column=2)
+        withdraw = Button(frame ,text = "Money Withdrawal", command= lambda: self.withdrawframe())
+        withdraw.grid(row = 3, column=2)
 
         statement = Button(frame ,text = "Print Statement", command= lambda: self.printstatement())
-        statement.grid(row = 5, column=2)
+        statement.grid(row = 3, column=4)
 
-        transfer = Button(frame ,text = "Transfer Money", command= lambda: self.transfer())
-        transfer.grid(row = 6, column=2)
+        transfer = Button(frame ,text = "Transfer Money", command= lambda: self.transferframe())
+        transfer.grid(row = 4, column=2)
 
         accountclose = Button(frame ,text = "Account Closure", command= lambda: self.accountclose())
-        accountclose.grid(row = 7, column=2)
+        accountclose.grid(row = 4, column=4)
 
         logout = Button(frame ,text = "Customer Logout", command= lambda: self.logout())
-        logout.grid(row = 8, column=2)
+        logout.grid(row = 5, column=2)
 
+        back = Button(frame ,text = "back", command= lambda: self.showsignin())
+        back.grid(row = 6, column=3)
+        
         self.Frames["submenu"] = frame
 
     def displaystatement(self, acctno, name, age, address, TOA, balance):
@@ -178,7 +191,91 @@ class UI:
         back.grid(row = 3, column=2)
 
         self.Frames["closed"] = frame
+
+    def addressframe(self):
+        frame = Frame(self.Window, width=768, height=576 , bg="ivory")
+        frame.grid(row=0,column=0)
+        frame.grid_propagate(0)
+        Label(frame , text="address change" ,fg="red").grid(row=0 , column=1)
+
+        Label(frame , text="current address :").grid(row=2, column=0)
+
+        address = Entry(frame, width=20)
+        address.grid(row=2, column =2)
+
+        submit = Button(frame ,text = "submit", command= lambda: self.changeaddress(address.get()))
+        submit.grid(row = 3, column=1)
         
+        back = Button(frame ,text = "back", command= lambda: self.showsubmenu())
+        back.grid(row = 5, column=1)
+
+        self.Frames["address"] = frame
+
+    def depositframe(self):
+        frame = Frame(self.Window, width=768, height=576 , bg="ivory")
+        frame.grid(row=0,column=0)
+        frame.grid_propagate(0)
+        Label(frame , text="Deposit" ,fg="red").grid(row=0 , column=1)
+
+        Label(frame , text="Amount :").grid(row=2, column=0)
+
+        amount = Entry(frame, width=20)
+        amount.grid(row=2, column =2)
+
+        submit = Button(frame ,text = "submit", command= lambda: self.deposit(amount.get()))
+        submit.grid(row = 3, column=1)
+        
+        back = Button(frame ,text = "back", command= lambda: self.showsubmenu())
+        back.grid(row = 5, column=1)
+
+        self.Frames["deposit"] = frame
+
+    def withdrawframe(self):
+        frame = Frame(self.Window, width=768, height=576 , bg="ivory")
+        frame.grid(row=0,column=0)
+        frame.grid_propagate(0)
+        Label(frame , text="Withdrawal" ,fg="red").grid(row=0 , column=1)
+
+        Label(frame , text="Amount :").grid(row=2, column=0)
+
+        amount = Entry(frame, width=20)
+        amount.grid(row=2, column =2)
+
+        submit = Button(frame ,text = "submit", command= lambda: self.withdraw(amount.get()))
+        submit.grid(row = 3, column=1)
+        
+        back = Button(frame ,text = "back", command= lambda: self.showsubmenu())
+        back.grid(row = 5, column=1)
+
+        self.Frames["withdraw"] = frame
+
+    def transferframe(self):
+        frame = Frame(self.Window, width=768, height=576 , bg="ivory")
+        frame.grid(row=0,column=0)
+        frame.grid_propagate(0)
+        Label(frame , text="Transfer" ,fg="red").grid(row=0 , column=1)
+
+        Label(frame , text="Amount :").grid(row=2, column=0)
+        Label(frame , text="actno :").grid(row=3, column=0)
+        
+        amount = Entry(frame, width=20)
+        amount.grid(row=2, column =2)
+        
+        accountno = Entry(frame, width=20)
+        accountno.grid(row=3, column =2)
+
+        submit = Button(frame ,text = "submit", command= lambda: self.transfer(amount.get(),accountno.get()))
+        submit.grid(row = 3, column=1)
+        
+        back = Button(frame ,text = "back", command= lambda: self.showsubmenu())
+        back.grid(row = 5, column=1)
+
+        self.Frames["tranfer"] = frame
+
+        
+    def showmainframe(self):
+        self.Frames["main"].tkraise()
+    
     def showsignup(self,event):
         self.Frames["signup"].tkraise()
 
@@ -192,10 +289,22 @@ class UI:
         self.Frames["submenu"].tkraise()
 
     def showdisplay(self):
-        self.Frames["dislay"].tkraise()
+        self.Frames["display"].tkraise()
 
     def showclosedacct(self):
         self.Frames["closed"].tkraise()
+
+    def showaddress(self):
+        self.Frames["address"].tkraise()
+
+    def showdeposit(self):
+        self.Frames["deposit"].tkraise()
+
+    def showwithdraw(self):
+        self.Frames["withdraw"].tkraise()
+
+    def showtransfer(self):
+        self.Frames["tranfer"].tkraise()
 
     def loginto(self,username,password):
         db = DB()
@@ -214,18 +323,19 @@ class UI:
             self.showclosedacct()
             
 
-    def changeaddress(self):
-        address = input("enter the address")
+    def changeaddress(self,address):
+        #address = input("enter the address")
         db = DB()
         db.updateaddress(address,self.accountno)
+        
 
-    def deposit(self):
-        amount = input("enter the amount")
+    def deposit(self,amount):
+        #amount = input("enter the amount")
         db = DB()
         db.deposit(amount,self.accountno)
 
-    def withdraw(self):
-        amount = input("enter the amount")
+    def withdraw(self,amount):
+        #amount = input("enter the amount")
         db = DB()
         db.withdraw(amount,self.accountno)
 
@@ -244,9 +354,9 @@ class UI:
         self.showdisplay()
        
 
-    def transfer(self):
-        amount = input("enter the amount")
-        actno = input("enter the accountno of reciever account")
+    def transfer(self,amount,actno):
+        #amount = input("enter the amount")
+        #actno = input("enter the accountno of reciever account")
         db = DB()
         db.withdraw(amount,self.accountno)
         db.deposit(amount,actno)
@@ -280,7 +390,8 @@ class DB:
         print (username)
         print (password)
         print (TOA)
-        self.accountno = self.accountno + 1
+        self.accountno = random.randint(1000,10000)
+        print 
         balance = 0
         sql="insert into customer values(:accountno,:name,:age,:address,:username, :password ,:TOA ,:balance)"
         self.cursor.execute(sql,{'accountno':self.accountno,'name':name,'age':age,'address':address,'username':username,'password':password,'TOA':TOA,'balance': balance})
@@ -288,23 +399,28 @@ class DB:
         
     def deposit(self,amount,accountno):
         self.cursor.execute("select balance from customer where accountno = :accountno",{'accountno':accountno})
-        balance = self.cursor.fetchall()
-        self.cursor.execute("update customer set balance = :balance where accountno = :accountno",{'balance':balance+amount,'accountno':accountno})
+        record = self.cursor.fetchall()
+        balance = int(record[0][0])
+        amount = int(amount)
+        self.cursor.execute("update customer set balance = :balance where accountno = :accountno",{'balance':balance + amount,'accountno':accountno})
         self.con.commit()
 
     def withdraw(self,amount,accountno):
         self.cursor.execute("select balance from customer where accountno = :accountno",{'accountno':accountno})
         balance = self.cursor.fetchall()
+        balance = int(record[0][0])
+        amount =int(amount)
         self.cursor.execute("update customer set balance = :balance where accountno = :accountno",{'balance':balance-amount,'accountno':accountno})
         self.con.commit()
         
     def login(self,username ,password):
         self.cursor.execute("select username from customer")
         users = self.cursor.fetchall()
-        if (username in users[0]):
+        usernametuple = (username,)
+        print usernametuple
+        if (usernametuple in users):
             self.cursor.execute("select password from customer where username = :username",{'username':username})
             passwrd = self.cursor.fetchall()
-            print (passwrd)
             if (passwrd[0][0] == password):
                 self.cursor.execute("select accountno from customer where username = :username",{'username':username})
                 accountno = self.cursor.fetchall()
@@ -315,8 +431,7 @@ class DB:
         else:
             return 0
     def display(self,accountno):
-        record = []
-        self.cursor.execute("select *  from customer")
+        self.cursor.execute("select *  from customer where accountno = :accountno",{'accountno': accountno})
         record = self.cursor.fetchall()
         return (record)
     def updateaddress(self,address,accountno):
